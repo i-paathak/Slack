1432ec4a235b7b8a6d9ca655f388b30c347ef5e4
Shivam Pathak <126328890+i-paathak@users.noreply.github.com>
2023-06-22 10:53:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,52 @@
 import requests
 import pandas as pd
 import dash
-from dash import html,dcc
+from dash import html, dcc
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 from io import StringIO
 from dash_table import DataTable
 import json
 import os
+import re
+import yfinance as yf
+from telethon.sync import TelegramClient
+from secret import api_hash,api_id,group_username
 
-API_KEY = os.environ['ALPHA_VANTAGE'] 
+API_KEY = os.environ['ALPHA_VANTAGE']
 
-# Fetching stock symbols and their names
-url = "https://www.alphavantage.co/query?function=LISTING_STATUS&apikey={API_KEY}&datatype=csv"
-response = requests.get(url)
-data = pd.read_csv(StringIO(response.text))
-data = data.dropna(subset=['symbol', 'name'])  # Drop rows with missing values
-symbols = data[['symbol', 'name']].set_index('symbol').to_dict()['name']
+# Fetching stock symbols from Telegram messages
+with TelegramClient('anon', api_id, api_hash) as client:
+    # Get all the messages
+    messages = client.get_messages(group_username, limit=100)
+
+    # List to store symbols
+    symbols = []
+
+    # Iterate over each message
+    for message in messages:
+        # Use regex to find potential symbols from length 2 to 5.
+        potential_symbols = re.findall(r'\b[A-Z]{2,5}\b', message.text)
+
+        # Verifying if they are symbols by passing them in yahooFinance.Ticker func
+        for symbol in potential_symbols:
+            try:
+                data = yf.Ticker(symbol)
+                # If the symbol exists, append it to the list
+                if data.info['symbol'] == symbol:
+                    symbols.append(symbol)
+            except:
+                pass
+
+##Rest of them is pretty much same
 
 # Creating a Dash application
 app = dash.Dash(__name__)
+
+# Fetching stock symbols and their names
+symbols_data = pd.DataFrame({'symbol': symbols})
+symbols_data['name'] = symbols_data['symbol'].apply(lambda symbol: yf.Ticker(symbol).info.get('longName', symbol))
+symbols = symbols_data.set_index('symbol').to_dict()['name']
 
 app.layout = html.Div(style={'textAlign': 'center'}, children=[
     html.H1('Stock Data Visualization'),
@@ -35,15 +62,19 @@ app.layout = html.Div(style={'textAlign': 'center'}, children=[
 
 @app.callback(
     [Output('graph', 'figure'),
-     Output('quote', 'children'),Output('news-container', 'children')],
+     Output('quote', 'children'),
+     Output('news-container', 'children')],
     [Input('dropdown', 'value')]
-
 )
 def update_output(value):
     # Fetching time series data
     url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={value}&apikey={API_KEY}&datatype=csv"
     response = requests.get(url)
     data = pd.read_csv(StringIO(response.text))
+
+    if 'timestamp' not in data.columns:
+        return go.Figure(), html.P('No data found for the specified symbol.'), html.Div()
+
     data['timestamp'] = pd.to_datetime(data['timestamp'])
     data.set_index('timestamp', inplace=True)
     data.sort_index(ascending=True, inplace=True)
@@ -70,6 +101,7 @@ def update_output(value):
             'fontWeight': 'bold'
         }
     )
+
     # Fetching News Feed data
     url = f"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&symbol={value}&apikey={API_KEY}&datatype=csv"
     response = requests.get(url)
